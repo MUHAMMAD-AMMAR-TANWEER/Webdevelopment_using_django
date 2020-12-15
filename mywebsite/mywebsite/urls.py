24b558e520ec378428f62b8e4257dee6792297f6
Muhammadammar <ammartanweer7370@gmail.com>
2020-12-15 08:00:48 +0500

    These are code harry video code

---
+++ mywebsite/mywebsite/urls.py
@@ -0,0 +1,49 @@
+"""mywebsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.index, name = 'index'),#This is connecting to the views file check what is in the index function
+#     path('about/', views.about, name = 'about'),
+#     path('more/',views.test_task, name='more'),
+#     path('task2/',views.task2, name = 'task2'),
+#     #This is going to get the about function in views.py file
+# ] This code is for video 6 now time for video 7 in which we add pipe
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',views.index, name = 'index'),
+#     path('removepunc', views.removepunc, name='rempun'),
+#     path('capitalizefirst', views.capfirst, name='capfirst'),
+#     path('newlineremove', views.newlineremove, name='newlineremove'),
+#     path('spaceremove', views.spaceremove, name='spaceremove'),
+#     path('charcount', views.charcount, name='charcount'),
+# ]
+#Above code is of video 7 and I also completed the challange now it's time to add templates in video 8
+#first go to settings.py and 'DIRS': ["templates"], do this ten we are able to addd templates
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('removepunc', views.removepunc, name='rempun'),
+    path('capitalizefirst', views.capfirst, name='capfirst'),
+    path('newlineremove', views.newlineremove, name='newlineremove'),
+    path('spaceremove', views.spaceremove, name='spaceremove'),
+    path('charcount', views.charcount, name='charcount'),
+
+]
---
+++ mywebsite/mywebsite/views.py
@@ -0,0 +1,70 @@
+# I created this file for editing and making the website
+#to run the server type python manage.py runserver
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+#to get the http response from server we use this
+# def index(request):#We have to give a default parameter to the function
+#     return HttpResponse("My name is Ammar")#We need to pass in this format
+#
+# def about(request):
+#     return HttpResponse("I am Great in Machine learning")#This is second function which also displays on the page now here comes the third one
+# def test_task(request):#We can display text files using this function so the challange has been solved
+#     f = open('mywebsite/ammar.txt','r')
+#     file_content = f.read()
+#     f.close()
+#     return HttpResponse(file_content, content_type="text/plain")
+# #Now another challange occurs that is we have to put five links through which we can redirect
+# def task2(request):#I put five links on my task2 page
+#     return HttpResponse('''<h1>Muhammad Ammar</h1><a href="http://techphoenix.epizy.com/index/?i=1">Our website</a>
+#     <a href="https://skrollo.com/">2 website</a>
+#     <a href="https://pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html#sphx-glr-beginner-blitz-tensor-tutorial-py">3 website</a>
+#     <a href="https://www.tensorflow.org/">4 website</a>
+#     <a href="https://academy.tcm-sec.com/">5 website</a>''')
+#############Above task is for video 6 now we will go to video 7
+#We are building a documnet parser website and I have a challenge to add buttons to this website which will redirct to home page
+# def index(request):
+#     return HttpResponse("Home")
+#
+# def removepunc(request):
+#     return HttpResponse('''<h1>remove punc</h1><a href="http://127.0.0.1:8000/"><button>Return Home</button></a>''')
+#
+# def capfirst(request):
+#     return HttpResponse('''<h1>Capitalize letters</h1><a href="http://127.0.0.1:8000/"><button>Return Home</button></a>''')
+#
+# def newlineremove(request):
+#     return HttpResponse('''<h1>line remover</h1><a href="http://127.0.0.1:8000/"><button>Return Home</button></a>''')
+#
+#
+# def spaceremove(request):
+#     return HttpResponse('''<h1>Space Remover</h1><a href="http://127.0.0.1:8000/"><button>Return Home</button></a>''')
+#
+# def charcount(request):
+#     return HttpResponse('''<h1>Character Count</h1><a href="http://127.0.0.1:8000/"><button>Return Home</button></a>''')
+#Above code is for video 7 now we are at video 8
+def index(request):
+    return render(request, 'index.html')
+def removepunc(request):
+
+    #now we get the response which user submit in the form through below command
+    djreturns = request.GET.get('text','defaut')
+    print(djreturns)
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("newline remove first")
+
+
+def spaceremove(request):
+    return HttpResponse("space remover back")
+
+def charcount(request):
+    return HttpResponse("charcount ")
